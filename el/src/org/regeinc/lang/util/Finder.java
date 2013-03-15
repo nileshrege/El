8bb5531cb2309aeb55d01b3eb57d4c498331c71b
nileshrege <rege.nilesh@regeinc.org>
2013-03-15 17:15:15 +0530

    working on validations

---
+++ el/src/org/regeinc/lang/util/Finder.java
@@ -0,0 +1,180 @@
+package org.regeinc.lang.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.regeinc.lang.el.Association;
+import org.regeinc.lang.el.Constraint;
+import org.regeinc.lang.el.Contract;
+import org.regeinc.lang.el.Entity;
+import org.regeinc.lang.el.LineStatement;
+import org.regeinc.lang.el.LocalVariableBinding;
+import org.regeinc.lang.el.MethodDeclaration;
+import org.regeinc.lang.el.MethodDefinition;
+import org.regeinc.lang.el.Model;
+import org.regeinc.lang.el.Parameter;
+import org.regeinc.lang.el.Type;
+import org.regeinc.lang.el.TypeRef;
+
+interface Criteria {
+	boolean isSatisfiedBy(EObject context);
+}
+
+class TypeCriteria implements Criteria{
+	public boolean isSatisfiedBy(EObject context) {
+		return context instanceof Type;
+	}
+}
+
+class ContractCriteria implements Criteria{
+	public boolean isSatisfiedBy(EObject context) {
+		return context instanceof Contract;
+	}
+}
+
+class EntityCriteria implements Criteria{
+	public boolean isSatisfiedBy(EObject context) {
+		return context instanceof Entity;
+	}
+}
+
+class MethodDeclarationCriteria implements Criteria{
+	public boolean isSatisfiedBy(EObject context) {
+		return context instanceof MethodDeclaration;
+	}
+}
+
+class MethodDefinitionCriteria implements Criteria{
+	public boolean isSatisfiedBy(EObject context) {
+		return context instanceof MethodDefinition;
+	}
+}
+
+class ConstraintCriteria implements Criteria{
+	public boolean isSatisfiedBy(EObject context) {
+		return context instanceof Constraint;
+	}
+}
+
+class ConditionCriteria implements Criteria{
+	public boolean isSatisfiedBy(EObject context) {
+		return context instanceof ConditionCriteria;
+	}
+}
+
+class LineStatementCriteria implements Criteria{
+	public boolean isSatisfiedBy(EObject context) {
+		return context instanceof LineStatement;
+	}
+}
+
+public class Finder {
+	
+	public static EObject lookUp(EObject context, Criteria criteria) {
+		if (criteria.isSatisfiedBy(context)) {
+			return context;
+		} else if (!(context.eContainer() instanceof Model)) {
+			return lookUp(context.eContainer(), criteria);
+		} else {
+			return null;
+		}
+	}
+
+	public static Type type(EObject context) {
+		Type type = (Entity) lookUp(context, new TypeCriteria());
+		return type;
+	}
+
+	public static List<TypeRef> associations(EObject context) {
+		List<TypeRef> allTypeRef = new ArrayList<TypeRef>();
+		Entity entity = (Entity) lookUp(context, new EntityCriteria());
+		if (entity != null) {
+			for (Association association : entity.getAllAssociation()) {
+				allTypeRef.add(association.getSpecificTypeRef().getTypeRef());
+			}
+		}
+		return allTypeRef;
+	}
+
+	public static MethodDeclaration methodDeclaration(EObject context) {
+		Entity entity = (Entity) lookUp(context, new EntityCriteria());
+		if (entity != null) {
+			for (MethodDefinition methodDefinition : entity.getAllMethodDefinition()) {
+				if(methodDefinition.getMethodDeclaration().equals(context))
+					return methodDefinition.getMethodDeclaration();
+			}
+		}
+		return null;
+	}
+	
+	public static List<MethodDeclaration> allMethodDeclaration(EObject context) {
+		List<MethodDeclaration> allMethodDeclaration = new ArrayList<MethodDeclaration>();
+		Contract contract = (Contract) lookUp(context, new ContractCriteria());
+		if (contract != null) {
+			for (MethodDeclaration declaration : contract.getAllMethodDeclaration()) {
+				allMethodDeclaration.add(declaration);
+			}
+		}else{
+			Entity entity = (Entity) lookUp(context, new EntityCriteria());
+			if (entity != null) {
+				for (MethodDefinition definition : entity.getAllMethodDefinition()) {
+					allMethodDeclaration.add(definition.getMethodDeclaration());
+				}
+			}	
+		}		
+		return allMethodDeclaration;
+	}
+	
+	public static List<MethodDefinition> allMethodDefinition(EObject context) {
+		List<MethodDefinition> allMethodDefinition = new ArrayList<MethodDefinition>();
+		Entity entity = (Entity) lookUp(context, new EntityCriteria());
+		if (entity != null) {
+			for (MethodDefinition definition : entity.getAllMethodDefinition()) {
+				allMethodDefinition.add(definition);
+			}
+		}		
+		return allMethodDefinition;
+	}
+
+	public static List<TypeRef> allParameter(EObject context) {
+		class ParameterFinder {
+			List<TypeRef> find(Parameter parameter) {
+				List<TypeRef> allTypeRef = new ArrayList<TypeRef>();
+				allTypeRef.add(parameter.getSpecificTypeRef().getTypeRef());
+				if(parameter.isList()){
+					allTypeRef.addAll(find(parameter.getNext()));
+				}
+				return allTypeRef;
+			}
+		}
+
+		List<TypeRef> allTypeRef = new ArrayList<TypeRef>();
+		MethodDefinition methodDefinition = (MethodDefinition) lookUp(context, new MethodDefinitionCriteria());
+		MethodDeclaration methodDeclaration = methodDefinition.getMethodDeclaration();
+		if (methodDeclaration != null) {
+			Parameter parameter = methodDeclaration.getParameter();
+			allTypeRef.addAll(new ParameterFinder().find(parameter));
+		}
+		return allTypeRef;
+	}
+
+	public static List<TypeRef> allLocalVariable(EObject context) {
+		class LocalVariableBindingFinder{
+			List<TypeRef> find(LocalVariableBinding localVariableBinding) {
+				List<TypeRef> allTypeRef = new ArrayList<TypeRef>();
+				allTypeRef.add(localVariableBinding.getLocalVariableDeclaration().getSpecificTypeRef().getTypeRef());
+				if(localVariableBinding.isList()){
+					allTypeRef.addAll(find(localVariableBinding.getNext()));
+				}
+				return allTypeRef;
+			}
+		}
+		List<TypeRef> allTypeRef = new ArrayList<TypeRef>();
+		LineStatement lineStatement = (LineStatement) lookUp(context, new LineStatementCriteria());
+		if (lineStatement != null && lineStatement.getLocalVariableBinding()!= null) {
+			allTypeRef.addAll(new LocalVariableBindingFinder().find(lineStatement.getLocalVariableBinding()));
+		}
+		return allTypeRef;
+	}
+}
--- el/src/org/regeinc/lang/validation/ElJavaValidator.java
+++ el/src/org/regeinc/lang/validation/ElJavaValidator.java
@@ -1,13 +1,267 @@
 package org.regeinc.lang.validation;
- 
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.xtext.validation.Check;
+import org.regeinc.lang.el.Association;
+import org.regeinc.lang.el.Contract;
+import org.regeinc.lang.el.DotMethodCall;
+import org.regeinc.lang.el.ElPackage;
+import org.regeinc.lang.el.Element;
+import org.regeinc.lang.el.Entity;
+import org.regeinc.lang.el.Import;
+import org.regeinc.lang.el.Instance;
+import org.regeinc.lang.el.LocalVariableDeclaration;
+import org.regeinc.lang.el.MethodDeclaration;
+import org.regeinc.lang.el.MethodDefinition;
+import org.regeinc.lang.el.Model;
+import org.regeinc.lang.el.Parameter;
+import org.regeinc.lang.el.Type;
+import org.regeinc.lang.el.TypeRef;
+import org.regeinc.lang.util.Finder;
+
+/**
+ * this class checks for
+ * uniqueness
+ * right thing at right place
+ * completeness
+ * 
+ * @author nilesh
+ *
+ */
 public class ElJavaValidator extends AbstractElJavaValidator {
 
-//	@Check
-//	public void checkGreetingStartsWithCapital(Greeting greeting) {
-//		if (!Character.isUpperCase(greeting.getName().charAt(0))) {
-//			warning("Name should start with a capital", MyDslPackage.Literals.GREETING__NAME);
-//		}
-//	}
+	@Check
+	public void checkDuplicateImport(Import importStatement){
+		for(Element element: ((Model)importStatement.eContainer()).getAllElement()){
+			for(Import imp:element.getAllImport()){
+				if(imp.equals(importStatement)){
+					continue;
+				}
+				if(imp.getImportedNamespace().equals(importStatement.getImportedNamespace())){
+					warning("duplicate import", ElPackage.eINSTANCE.eContainingFeature());	
+				}	
+			}			
+		}
+	}
 
+	@Check
+	public void checkDuplicateAssociation(Association association){
+		List<Association> allAssociation = ((Entity)association.eContainer()).getAllAssociation();
+		for(Association nextAssociation: allAssociation){
+			if(association.equals(nextAssociation)){
+				continue;
+			}else{
+				if(association.getSpecificTypeRef().getTypeRef().getName().equals(nextAssociation.getSpecificTypeRef().getTypeRef().getName())){
+					error("duplicate association", ElPackage.eINSTANCE.eContainingFeature());
+					break;
+				}
+			}
+		}
+	}
+
+	@Check
+	public void checkDuplicateState(org.regeinc.lang.el.State state){
+		List<org.regeinc.lang.el.State> allState = ((Entity)state.eContainer()).getAllState();
+		for(org.regeinc.lang.el.State existingState: allState){
+			if(existingState.equals(state)){
+				continue;
+			}else{
+				if(existingState.getName().equals(state.getName())){
+					error("duplicate state", ElPackage.eINSTANCE.eContainingFeature());
+					break;
+				}
+			}
+		}
+	}
+
+	@Check
+	public void checkDuplicateMethodDeclaration(MethodDeclaration newMethodDeclaration){
+		if(newMethodDeclaration.eContainer() instanceof Contract){
+			for(MethodDeclaration existingMethodDeclaration: ((Contract)newMethodDeclaration.eContainer()).getAllMethodDeclaration()){
+				if(existingMethodDeclaration.equals(newMethodDeclaration)){
+					continue;
+				}
+				if(isSameMethodDeclaration(existingMethodDeclaration,newMethodDeclaration)){		
+					error("duplicate method", ElPackage.eINSTANCE.eContainingFeature());
+					break;
+				}
+			}
+		}else if(newMethodDeclaration.eContainer() instanceof Entity){
+			for(MethodDefinition methodDefinition: ((Entity)newMethodDeclaration.eContainer().eContainer()).getAllMethodDefinition()){
+				MethodDeclaration existingMethodDeclaration = methodDefinition.getMethodDeclaration();
+				if(existingMethodDeclaration.equals(newMethodDeclaration)){
+					continue;
+				}else{
+					if(isSameMethodDeclaration(existingMethodDeclaration,newMethodDeclaration)){		
+						error("duplicate method", ElPackage.eINSTANCE.eContainingFeature());
+						break;
+					}
+				}
+			}
+		}
+	}
+	
+	boolean isSameMethodDeclaration(MethodDeclaration first, MethodDeclaration second){
+		if(isSameMethodName(first, second)){ // same name
+			if(first.getParameter()!=null && second.getParameter()!=null){
+				if(isSameParameter(first.getParameter(), second.getParameter())){
+					return true;
+				}
+			}	
+		}
+		return false;
+	}
+	
+	boolean isSameMethodName(MethodDeclaration first, MethodDeclaration second){
+		return first.getName().equals(second.getName());
+	}
+
+	boolean isSameParameter(Parameter first, Parameter second){
+		Type firstType = first.getSpecificTypeRef().getTypeRef().getType();
+		Type secondType = second.getSpecificTypeRef().getTypeRef().getType(); 
+		if(isSameType(firstType, secondType)){
+			if(first.isList() && second.isList()){
+				return isSameParameter(first.getNext(), second.getNext());
+			}
+			return true;
+		}
+		return false;
+	}
+	
+	boolean isSameType(Type first, Type second){
+		String firstFullName = ((Element)first.eContainer()).getPkg().getName()+"."+first.getName();
+		String secondFullName = ((Element)second.eContainer()).getPkg().getName()+"."+first.getName();
+		
+		return firstFullName.equals(secondFullName);
+	}
+	
+	@Check
+	public void checkClassWithAbstractMethodBeDeclaredAbstract(Entity entity){
+		for(MethodDefinition methodDefinition : entity.getAllMethodDefinition()){
+			if(methodDefinition.isABSTRACT() && !entity.isABSTRACT()){
+				error("class with abstract method must be declared abstract", ElPackage.eINSTANCE.eContainingFeature());
+				break;
+			}	
+		}		
+	}
+	
+	@Check
+	public void checksForInstanceMethod(MethodDefinition methodDefinition){
+		if(methodDefinition.getMethodDeclaration().getName().equals("instance")){
+			if(!(methodDefinition.getMethodDeclaration().getReturnType()!=null) 
+					|| !(isOfType(methodDefinition.getMethodDeclaration().getReturnType().getType(),(Type)methodDefinition.eContainer()))){
+				error("instance method must return instance of the enclosing class or subclass", ElPackage.eINSTANCE.eContainingFeature());
+			}
+			List<TypeRef> identities = identities(((Entity)methodDefinition.eContainer()).getAllAssociation());
+			if(identities.size()>0 && methodDefinition.getMethodDeclaration().getParameter() == null){
+				error("instance method must declare all identity fields", ElPackage.eINSTANCE.eContainingFeature());
+			}else{
+				List<TypeRef> result = new ArrayList<TypeRef>();
+				result.addAll(identities);
+				for(TypeRef identity : identities){
+					Parameter param = methodDefinition.getMethodDeclaration().getParameter();
+					if(param.getSpecificTypeRef().getTypeRef().getType().getName().equals(identity.getType().getName())){
+						result.remove(identity);
+					}
+				}
+				if(result.size()>0){
+					error("instance method must declare all identity fields", ElPackage.eINSTANCE.eContainingFeature());
+				}
+			}
+			if(!methodDefinition.getVisibility().toString().equals("public")){
+				error("instance method must be declared public", ElPackage.eINSTANCE.eContainingFeature());
+			}
+		}
+	}
+	
+	boolean isOfType(Type source, Type destination){
+		boolean flag = false;
+		if(destination.getName().equals(source.getName())){
+			flag = true;
+		}else{
+			if(source.getType()!=null){
+				flag = isOfType(source.getType(), destination);
+			}
+		}
+		return flag;
+	}
+	
+	List<TypeRef> identities(List<Association> associationList){
+		List<TypeRef> identityList = new ArrayList<TypeRef>();
+		for(Association association: associationList){
+			if(association.isIDENTITY()){
+				identityList.add(association.getSpecificTypeRef().getTypeRef());
+			}
+		}
+		return identityList;
+	}
+	
+	@Check
+	public void checkTypedParametersOnlyAllowedForPrivateMethods(MethodDeclaration methodDeclaration){
+		if(methodDeclaration.eContainer() instanceof Contract || !((MethodDefinition)methodDeclaration.eContainer()).getVisibility().toString().equals("private")){
+			if(methodDeclaration.getParameter()!=null){
+				if(isSpecificTypeParam(methodDeclaration.getParameter())){
+					error("typed parameters only allowed in private methods of class", ElPackage.eINSTANCE.eContainingFeature());	
+				}
+			}
+		}
+	}
+	
+	boolean isSpecificTypeParam(Parameter parameter){
+		if(parameter.getSpecificTypeRef().getOrTypePrefix()==null && parameter.isList()){
+			return isSpecificTypeParam(parameter.getNext());
+		}
+		return false;
+	}
+	
+	@Check
+	public void checkAbstractMethodsNotDeclaredPrivate(MethodDefinition methodDefinition){
+		if(!(methodDefinition.eContainer() instanceof Contract)){
+			if(methodDefinition.isABSTRACT()){
+				if(methodDefinition.getVisibility().toString().equals("private")){
+					error("abstract methods can not be declared private", ElPackage.eINSTANCE.eContainingFeature());	
+				}
+			}
+		}
+	}
+	
+	@Check
+	public void checkTypeMatches(LocalVariableDeclaration localVariableDeclaration){
+		Type lhs = localVariableDeclaration.getSpecificTypeRef().getTypeRef().getType();
+		Type rhs = instanceType(localVariableDeclaration.getInstance());
+
+		Element element = (Element)lhs.eContainer();
+		String lhsn = element.getPkg()+"."+lhs.getName();
+		String rhsn = element.getPkg()+"."+rhs.getName();
+		if(!lhsn.equals(rhsn)){
+			error("type mismatch", ElPackage.eINSTANCE.eContainingFeature());
+		}
+	}
+	
+	Type instanceType(Instance instance){
+		Type result = null;
+		if(instance.getDotMethodCall()!=null){
+			MethodDeclaration methodDeclaration = lastMethodCall(instance.getDotMethodCall()).getMethodCall().getMethodDeclaration();
+			if(methodDeclaration.getReturnType()!=null){
+				result = methodDeclaration.getReturnType().getType();
+			}
+		}else if(instance.getStateOrVariable()!=null && instance.getStateOrVariable() instanceof TypeRef){
+			result = ((TypeRef)instance.getStateOrVariable()).getType();
+		}else if(instance.getLiteral()!=null){
+			if(instance.getLiteral().getTHIS()!=null){
+				result = Finder.type(instance);
+			}
+		}
+		return result;
+	}
+	
+	DotMethodCall lastMethodCall(DotMethodCall dotMethodCall){
+		if(dotMethodCall.getDotMethodCall()!=null){
+			return lastMethodCall(dotMethodCall);
+		}else{
+			return dotMethodCall;
+		}
+	}
 }
